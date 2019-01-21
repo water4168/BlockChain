aa86c7f7af4997bfc1da148c10fd6e3c22ea8eca
water <tianming581@gmail.com>
2019-01-21 15:27:40 +0800

    update setting
    update setting

--- 用例管理平台/TestCasePla/TestCasePla/settings.py
+++ 用例管理平台/TestCasePla/TestCasePla/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
